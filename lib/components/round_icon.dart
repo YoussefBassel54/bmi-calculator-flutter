36cbe68bf8855cfb0cd8a8e25ea2e862ef5dd30d
YoussefBassel54 <53179104+YoussefBassel54@users.noreply.github.com>
2022-01-04 13:26:11 +0200

    Add files via upload

---
+++ lib/components/round_icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  const RoundIconButton({@required this.icon, @required this.onPressed});
+  final IconData icon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      elevation: 6,
+      constraints: BoxConstraints.tightFor(
+        width: 56,
+        height: 56,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+      onPressed: onPressed,
+      child: Icon(icon),
+    );
+  }
+}
